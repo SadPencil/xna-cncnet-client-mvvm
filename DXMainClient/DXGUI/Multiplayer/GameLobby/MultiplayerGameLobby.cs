dd595090ffe8c807884f34b08e3efb30b7482a9c
SadPencil <sadpencil@outlook.com>
2026-05-30 16:41:23 +0800

    check: MultiplayerGameLobby business logic fully migrated to ViewModel

--- DXMainClient/DXGUI/Multiplayer/GameLobby/MultiplayerGameLobby.cs
+++ DXMainClient/DXGUI/Multiplayer/GameLobby/MultiplayerGameLobby.cs
@@ -23,7 +23,7 @@ namespace DTAClient.DXGUI.Multiplayer.GameLobby
     /// <summary>
     /// A generic base class for multiplayer game lobbies (CnCNet and LAN).
     /// </summary>
-    public abstract class MultiplayerGameLobby : GameLobbyBase, ISwitchable
+    public abstract class MultiplayerGameLobby : GameLobbyBase, ISwitchable // checked
     {
         private const int MAX_DICE = 10;
         private const int MAX_DIE_SIDES = 100;
