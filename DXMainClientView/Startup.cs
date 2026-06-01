28e6db960bb0efb6cdea0bda536f1379eb46ed7f
SadPencil <sadpencil@outlook.com>
2026-06-01 22:47:38 +0800

    Add headless option

--- DXMainClientView/Startup.cs
+++ DXMainClientView/Startup.cs
@@ -2,6 +2,7 @@
 using System.Text;
 
 using Avalonia;
+using Avalonia.Headless;
 
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,17 +13,26 @@ public class Startup
     [STAThread]
     public static void Run(ServiceProvider serviceProvider, string[] args)
     {
+        // TODO: move it to view model
         Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
         ViewConstants.ServiceProvider = serviceProvider;
 
-        BuildAvaloniaApp()
+        bool headless = args.Contains("--headless");
+
+        BuildAvaloniaApp(headless)
             .StartWithClassicDesktopLifetime(args);
     }
 
-    private static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
-            .UsePlatformDetect()
-            .WithInterFont()
-            .LogToTrace();
+    private static AppBuilder BuildAvaloniaApp(bool headless = false)
+    {
+        var app = AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .WithInterFont().LogToTrace();
+
+        if (headless)
+            app = app.UseHeadless(new AvaloniaHeadlessPlatformOptions { });
+
+        return app;
+    }
 }
