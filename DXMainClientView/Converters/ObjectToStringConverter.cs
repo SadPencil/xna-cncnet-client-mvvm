8fb604517d43c7d65bfd2fa0c4a8d6d0fe303368
SadPencil <sadpencil@outlook.com>
2026-06-02 00:06:59 +0800

    1

---
+++ DXMainClientView/Converters/ObjectToStringConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+using Avalonia.Data.Converters;
+
+namespace DXMainClientView.Converters;
+
+public class ObjectToStringConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value?.ToString() ?? string.Empty;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
