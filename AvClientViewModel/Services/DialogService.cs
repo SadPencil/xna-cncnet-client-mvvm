91001bc73d42f1f46e243db99b38db5d998f9ce3
SadPencil <sadpencil@outlook.com>
2026-06-03 10:00:29 +0800

    Update DialogService.cs

--- AvClientViewModel/Services/DialogService.cs
+++ AvClientViewModel/Services/DialogService.cs
@@ -8,19 +8,24 @@ using AvClientMvvmContract.Messages;
 
 using CommunityToolkit.Mvvm.Messaging;
 
+using Serilog;
+
 namespace AvClientViewModel.Services
 {
     public class DialogService
     {
         public async Task ShowOKDialog(string title, string message)
         {
+            Log.Information($"Showing OK dialog. Title: {title}, Message: {message}");
             _ = await WeakReferenceMessenger.Default.Send(new OKDialogAsyncRequestMessage(title, message));
         }
 
         public async Task<bool> ShowYesNoDialog(string title, string message)
         {
+            Log.Information($"Showing Yes/No dialog. Title: {title}, Message: {message}");
             var msg = new YesNoDialogAsyncRequestMessage(title, message);
             YesNoDialogResult result = await WeakReferenceMessenger.Default.Send(msg);
+            Log.Information($"Yes/No dialog closed. Title: {title}, Message: {message}, Result: {result.Result}");
             return result.Result;
         }
     }
