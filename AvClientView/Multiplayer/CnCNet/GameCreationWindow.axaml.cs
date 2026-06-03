39b9095afe8565109040ffd713d7127c4f1df538
SadPencil <sadpencil@outlook.com>
2026-06-04 02:07:23 +0800

    Fix: Color DataTemplate without x:DataType, GCW use PropertyChanged for DataContext

--- AvClientView/Multiplayer/CnCNet/GameCreationWindow.axaml.cs
+++ AvClientView/Multiplayer/CnCNet/GameCreationWindow.axaml.cs
@@ -1,3 +1,4 @@
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Data;
 
@@ -14,36 +15,45 @@ public partial class GameCreationWindow : UserControl, IGameCreationWindowView
     public GameCreationWindow()
     {
         InitializeComponent();
+        PropertyChanged += OnPropertyChanged;
     }
 
-    protected override void OnDataContextChanged(System.EventArgs e)
+    private void OnPropertyChanged(object? sender, AvaloniaPropertyChangedEventArgs e)
     {
-        base.OnDataContextChanged(e);
-        Log.Information("[LOG-View-GCW] OnDataContextChanged: new DC type={Type}",
+        if (e.Property != DataContextProperty) return;
+        Log.Information("[LOG-View-GCW] DataContext changed: new type={Type}",
             DataContext?.GetType().FullName ?? "null");
 
         if (!_bindingsWired && DataContext != null)
         {
-            if (cmbTunnel != null)
-            {
-                cmbTunnel.Bind(ComboBox.ItemsSourceProperty, new Binding("TunnelNames"));
-                cmbTunnel.Bind(ComboBox.SelectedIndexProperty, new Binding("SelectedTunnelIndex"));
-            }
-            if (cmbSkillLevel != null)
-            {
-                cmbSkillLevel.Bind(ComboBox.ItemsSourceProperty, new Binding("SkillLevelOptions"));
-                cmbSkillLevel.Bind(ComboBox.SelectedIndexProperty, new Binding("SelectedSkillLevel"));
-            }
-            _bindingsWired = true;
-            Log.Information("[LOG-View-GCW] ComboBox Bind() done on DataContext change");
+            WireBindings();
+        }
+    }
 
-            if (DataContext is IGameCreationWindowViewModel vm)
-            {
-                Log.Information("[LOG-View-GCW] VM state: TunnelNames.Count={T}, SkillLevelOptions.Count={S}, CanCreateGame={C}",
-                    vm.TunnelNames?.Count ?? -1, vm.SkillLevelOptions?.Count ?? -1, vm.CanCreateGame);
-                for (int i = 0; i < (vm.TunnelNames?.Count ?? 0) && i < 3; i++)
-                    Log.Information("[LOG-View-GCW] TunnelNames[{Idx}]={Name}", i, vm.TunnelNames![i]);
-            }
+    private void WireBindings()
+    {
+        Log.Information("[LOG-View-GCW] WireBindings: cmbTunnel={T}, cmbSkillLevel={S}",
+            cmbTunnel != null, cmbSkillLevel != null);
+
+        if (cmbTunnel != null)
+        {
+            cmbTunnel.Bind(ComboBox.ItemsSourceProperty, new Binding("TunnelNames"));
+            cmbTunnel.Bind(ComboBox.SelectedIndexProperty, new Binding("SelectedTunnelIndex"));
+        }
+        if (cmbSkillLevel != null)
+        {
+            cmbSkillLevel.Bind(ComboBox.ItemsSourceProperty, new Binding("SkillLevelOptions"));
+            cmbSkillLevel.Bind(ComboBox.SelectedIndexProperty, new Binding("SelectedSkillLevel"));
+        }
+        _bindingsWired = true;
+        Log.Information("[LOG-View-GCW] ComboBox Bind() done");
+
+        if (DataContext is IGameCreationWindowViewModel vm)
+        {
+            Log.Information("[LOG-View-GCW] VM: TunnelNames.Count={T}, SkillLevelOptions={S}, CanCreateGame={C}",
+                vm.TunnelNames?.Count ?? -1, vm.SkillLevelOptions?.Count ?? -1, vm.CanCreateGame);
+            for (int i = 0; i < (vm.TunnelNames?.Count ?? 0) && i < 3; i++)
+                Log.Information("[LOG-View-GCW] TunnelNames[{I}]={Name}", i, vm.TunnelNames![i]);
         }
     }
 
